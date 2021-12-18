a0d72e1351b2abd7300d009e768f4c23df0ab8e0
JonathanTW6455 <76730341+JonathanTW6455@users.noreply.github.com>
2021-12-18 15:58:34 +0800

    Update bakanono2.g.js

--- bakanono2.g.js
+++ bakanono2.g.js
@@ -1,85 +1,237 @@
 var userProperties = PropertiesService.getUserProperties();
 
 function doGet(e){
-  var temperature = e.parameter.r;
-  if (!temperature) {
+  var  temperature = e.parameter.t ;
+  var SPO2 = e.parameter.s;
+  var heartbeat = e.parameter.h;
+  if (!temperature){
+   
     return;
   }
-
+if (!SPO2){
+   
+    return;
+  }
+  if (!heartbeat){
+   
+    return;
+  }
   var nowDatetime = new Date().toLocaleString();
   if(temperature==0)
   {
-  userProperties.setProperty('temperatureText', nowDatetime  + " 錯誤，無法取得血氧濃度，請確認是否正確連接 " );
+  userProperties.setProperty('temperatureText', nowDatetime  + " 錯誤，無法取得溫度，請確認是否正確連接 " );
   }
-  if(temperature>95)
+  if(temperature<37.5)
   { 
-  userProperties.setProperty('temperatureText', nowDatetime  + " 您的血氧濃度是 " + temperature + " %");
+  userProperties.setProperty('temperatureText', nowDatetime  + " 您的體度是 " + temperature + "度");
   }
-  if(temperature<95 && temperature>0)
+  if(temperature>37.5 )
   {
-  userProperties.setProperty('temperatureText', nowDatetime  + " 您的血氧濃度是 " + temperature + " %"+"血氧濃度偏低，請留意身體狀況並盡速就醫");
+  userProperties.setProperty('temperatureText', nowDatetime  + " 您的體溫是 " + temperature + " 度"+"體溫偏高，請留意身體狀況並盡速就醫");
   }
- 
-  var returnText = temperature + " OK";
-  var textOutput = ContentService.createTextOutput(returnText)
-  return textOutput;
+  
+ if(SPO2==0)
+  {
+  userProperties.setProperty('SPO2Text', nowDatetime  + " 錯誤，無法取得血氧濃度，請確認是否正確連接 " );
+  }
+  if(SPO2>95)
+  { 
+  userProperties.setProperty('SPO2Text', nowDatetime  + " 您的血氧濃度是 " + SPO2 + " %");
+  }
+  if(SPO2<95 && SPO2>0)
+  {
+  userProperties.setProperty('SPO2Text', nowDatetime  + " 您的血氧濃度是 " + SPO2 + " %"+"血氧濃度偏低，請留意身體狀況並盡速就醫");
+  }
+  
+  if(heartbeat==0)
+  {
+  userProperties.setProperty('heartbeatText', nowDatetime  + " 錯誤，無法取得心率，請確認是否正確連接 " );
+  }
+  if(heartbeat>60 && heartbeat<100)
+  { 
+  userProperties.setProperty('heartbeatText', nowDatetime  + " 您的心率是 " + heartbeat );
+  }
+ if(heartbeat>0 && heartbeat<60)
+  {
+  userProperties.setProperty('heartbeatText', nowDatetime  + " 您的心率是 " + heartbeat + +"心率偏低，請留意身體狀況並盡速就醫");
+  }
+  if(heartbeat>100)
+  {
+  userProperties.setProperty('heartbeatText', nowDatetime  + " 您的心率是 " + heartbeat + +"心率偏高，請留意身體狀況並盡速就醫");
+  }
+  var returnTextT = temperature + " OK";
+  var textOutputT = ContentService.createTextOutput(returnTextT)
+  return textOutputT;
+  
+   var returnTextS = SPO2 + " OK";
+  var textOutputS = ContentService.createTextOutput(returnTextS)
+  return textOutputS;
+  
+   var returnTextH = heartbeat + " OK";
+  var textOutputH = ContentService.createTextOutput(returnTextH)
+  return textOutputH;
 }
 
 function doPost(e) {
   var msg = JSON.parse(e.postData.contents);
 
   // 取出 replayToken 和使用者送出的訊息文字
-  var replyToken = msg.events[0].replyToken;
-  var userMessage = msg.events[0].message.text;
+  var replyTokenT = msg.events[0].replyToken;
+  var userMessageT = msg.events[0].message.text;
+var replyTokenS = msg.events[0].replyToken;
+  var userMessageS = msg.events[0].message.text;
+var replyTokenH = msg.events[0].replyToken;
+  var userMessageH = msg.events[0].message.text;
 
-  if (typeof replyToken === 'undefined') {
+  if (typeof replyTokenT === 'undefined') {
     return;
   }
 
-  if (typeof keyWords === 'undefined') {
-    var keyWords = ["血氧濃度", "幾度", "熱不熱"];
+   if (typeof replyTokenT === 'undefined') {
+    return;
+  }
+  
+   if (typeof replyTokenH === 'undefined') {
+    return;
+  }
+  
+  if (typeof keyWordsT === 'undefined') {
+    var keyWordsT = ["溫度"];
   }
   else {
-    keyWords = keyWords.concat(["血氧濃度"]);
+    keyWordsT = keyWordsT.concat(["溫度"]);
   }
   
-  var returnText;
-  var hasKeyword = false;
+  if (typeof keyWordsS === 'undefined') {
+    var keyWordsS = ["血氧濃度"];
+  }
+  else {
+    keyWordsS = keyWordsS.concat(["血氧濃度"]);
+  }
   
-  if (userMessage) {
-    for (var i = 0; i < keyWords.length; i++) {
-      if (userMessage.indexOf(keyWords[i]) !== -1) {
-        hasKeyword = true;
+   if (typeof keyWordsH === 'undefined') {
+    var keyWordsH = ["心率"];
+  }
+  else {
+    keyWordsS = keyWordsH.concat(["心率"]);
+  }
+  
+  var returnTextT;
+  var returnTextS;
+  var returnTextH;
+  var hasKeywordT = false;
+  var hasKeywordS = false;
+  var hasKeywordH = false;
+  if (userMessageT) {
+    for (var i = 0; i < keyWordsT.length; i++) {
+      if (userMessageT.indexOf(keyWordsT[i]) !== -1) {
+        hasKeywordT = true;
         break;
       }
     }
   }
   
-  if (hasKeyword) {
+  if (userMessageS) {
+    for (var i = 0; i < keyWordsS.length; i++) {
+      if (userMessageS.indexOf(keyWordsS[i]) !== -1) {
+        hasKeywordS = true;
+        break;
+      }
+    }
+  }
+  
+  if (userMessageH) {
+    for (var i = 0; i < keyWordsH.length; i++) {
+      if (userMessageH.indexOf(keyWordsH[i]) !== -1) {
+        hasKeywordH = true;
+        break;
+      }
+    }
+  }
+  
+  if (hasKeywordT) {
     var temperatureText = userProperties.getProperty('temperatureText');
     if (temperatureText) {
-      returnText =  temperatureText;
+      returnTextT =  temperatureText;
     }
     else {
-      returnText = "抱歉我無法取得血氧濃度";
+      returnTextS = "抱歉我無法取得溫度";
     }
   }
   else {
-    returnText = getMisunderstandWords();
+    returnTextH = getMisunderstandWords();
+  }
+  
+   if (hasKeywordS) {
+    var SPO2Text = userProperties.getProperty('SPO2Text');
+    if (SPO2Text) {
+      returnTextS =  SPO2Text;
+    }
+    else {
+      returnTextS = "抱歉我無法取得血氧濃度";
+    }
+  }
+  else {
+    returnTextS = getMisunderstandWords();
+  }
+  
+  if (hasKeywordH) {
+    var heartbeatText = userProperties.getProperty('heartbeatText');
+    if (heartbeatText) {
+      returnTextH =  heartbeatText;
+    }
+    else {
+      returnTextH = "抱歉我無法取得心率";
+    }
+  }
+  else {
+    returnTextH = getMisunderstandWords();
   }
   
   var url = 'https://api.line.me/v2/bot/message/reply';
-  UrlFetchApp.fetch(url, {
+  UrlFetchAppT.fetch(url, {
       'headers': {
       'Content-Type': 'application/json; charset=UTF-8',
       'Authorization': 'Bearer ' + accessToken.trim(),
     },
     'method': 'post',
     'payload': JSON.stringify({
-      'replyToken': replyToken,
+      'replyToken': replyTokenT,
       'messages': [{
         'type': 'text',
-        'text': returnText,
+        'text': returnTextT,
+      }],
+    }),
+  });
+}
+
+ UrlFetchAppS.fetch(url, {
+      'headers': {
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization': 'Bearer ' + accessToken.trim(),
+    },
+    'method': 'post',
+    'payload': JSON.stringify({
+      'replyToken': replyTokenS,
+      'messages': [{
+        'type': 'text',
+        'text': returnTextS,
+      }],
+    }),
+  });
+}
+
+ UrlFetchAppH.fetch(url, {
+      'headers': {
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization': 'Bearer ' + accessToken.trim(),
+    },
+    'method': 'post',
+    'payload': JSON.stringify({
+      'replyToken': replyTokenH,
+      'messages': [{
+        'type': 'text',
+        'text': returnTextH,
       }],
     }),
   });
